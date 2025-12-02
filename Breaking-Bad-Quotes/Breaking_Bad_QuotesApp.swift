a3a505289966fb8e9d29e1c4f0c29d6f9e2a9117
ChenGuang-Py <58253607+ChenGuang-Py@users.noreply.github.com>
2025-12-02 15:37:44 +0100

    Initial Commit

---
+++ Breaking-Bad-Quotes/Breaking_Bad_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breaking_Bad_QuotesApp.swift
+//  Breaking-Bad-Quotes
+//
+//  Created by Guang on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct Breaking_Bad_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
